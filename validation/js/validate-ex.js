9c6880ded11182bffb2b321cfc4d087f417c8a2c
ppxj <16132451@qq.com>
2015-11-20 10:15:16 +0800

    delete some template

--- validation/js/validate-ex.js
+++
@@ -1,4 +0,0 @@
-// 用户名字符验证    
-jQuery.validator.addMethod("userName", function(value, element) {    
-  return this.optional(element) || /^[a-z_][a-z0-9_]{5,19}$/.test(value);    
-}, "用户名为6到20个小写英文字母、数字、下划线，不可以数字开头!");   
